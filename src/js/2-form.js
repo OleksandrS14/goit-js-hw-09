8fb1da669d80f3be2a92a2c421c1a1e19efc32f4
Oleksandr1451 <alekszzz154@gmail.com>
2024-09-21 19:36:35 +0300

    update index.html

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,13 +1,46 @@
-const formData = { email: "", message: "" };
-const form = document.querySelector(".feedback-form");
-form.addEventListener("input", onSaveData);
-function onSaveData (event) {
-  console.log(event.currentTarget.elements)
-  const email = event.currentTarget.elements.email.value.trim();
-  const message = event.currentTarget.elements.message.value.trim();
-  formData.email = email;
-  formData.message = message;
-  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+const FEEDBACK_FORM_LS = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+window.addEventListener('DOMContentLoaded', checkLocalStorage);
+form.addEventListener('input', handleFormValues);
+form.addEventListener('submit', handleFormSubmit);
+
+let formData = {
+  email: '',
+  message: '',
 };
 
+function handleFormValues() {
+  const emailValue = form.elements.email.value.trim();
+  const textAreaValue = form.elements.message.value.trim();
 
+  formData.email = emailValue;
+  formData.message = textAreaValue;
+
+  localStorage.setItem(FEEDBACK_FORM_LS, JSON.stringify(formData));
+}
+
+function checkLocalStorage() {
+  const valueLS = localStorage.getItem(FEEDBACK_FORM_LS);
+
+  if (valueLS) {
+    const saveData = JSON.parse(valueLS);
+    formData = { ...formData, ...saveData };
+
+    form.elements.email.value = formData.email || '';
+    form.elements.message.value = formData.message || '';
+  }
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  } else {
+    console.log(formData);
+  }
+  localStorage.removeItem(FEEDBACK_FORM_LS);
+  form.reset();
+  formData = { email: '', message: '' };
+}
